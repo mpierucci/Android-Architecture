7e9cf66fbe621982c2e4bb931cf961bf436ae392
Marco <marco1pierucci@gmail.com>
2020-02-22 20:56:06 +0000

    update minor libraries

--- viewmodel/build.gradle.kts
+++ viewmodel/build.gradle.kts
@@ -33,17 +33,17 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
     implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.1.0")
-    testImplementation("junit:junit:4.12")
+    implementation("androidx.core:core-ktx:1.2.0")
+    testImplementation("junit:junit:4.13")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
-    implementation("androidx.activity:activity-ktx:1.1.0-rc03")
-    implementation("androidx.fragment:fragment-ktx:1.2.0-rc03")
+    implementation("androidx.activity:activity-ktx:1.1.0")
+    implementation("androidx.fragment:fragment-ktx:1.2.2")
 }
 
 
 val artifactID = "viewmodel"
-val version = "0.1.1"
+val version = "0.1.2"
 
 publishing {
     publications {
